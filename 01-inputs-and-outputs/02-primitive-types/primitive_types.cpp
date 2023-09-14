8eb47eb7a7fd8269f49d9ec4561525d686352582
Gerard Garcia Grau <gerardgarciagrau2@gmail.com>
2023-09-14 22:32:48 +0200

    01-02 primitive types

---
+++ 01-inputs-and-outputs/02-primitive-types/primitive_types.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main () {
+
+    int integerType = 33;
+    float floatType = 55.55;
+    double doubleType = 16.3456;
+    char character = 'A';
+
+    cout<< integerType <<endl;
+    cout<< floatType <<endl;
+    cout<< doubleType <<endl;
+    cout<< character <<endl;
+
+    return 0;
+}
