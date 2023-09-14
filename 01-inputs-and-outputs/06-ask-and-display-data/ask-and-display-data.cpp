e8d35536222c09aa53644ddc490da5d4ae26229e
Gerard Garcia Grau <gerardgarciagrau2@gmail.com>
2023-09-14 23:10:45 +0200

    01-06 ask and display data (char array)

---
+++ 01-inputs-and-outputs/06-ask-and-display-data/ask-and-display-data.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main () {
+
+    int age;
+    char sex[10];
+    float height;
+
+    cout<<"Introduce your age> ";
+    cin>>age;
+
+    cout<<"Introduce your sex> ";
+    cin>>sex;
+
+    cout<<"Introduce your height> ";
+    cin>>height;
+
+    cout<<"Age: "<<age<<endl;
+    cout<<"Sex: "<<sex<<endl;
+    cout<<"Height: "<<height<<endl;
+
+    return 0;
+}
