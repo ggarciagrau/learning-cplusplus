fef7f0a069250665293e74775a7e2a2ed47ec9e7
Gerard Garcia Grau <gerardgarciagrau2@gmail.com>
2023-09-14 22:24:06 +0200

    01 - first program

---
+++ 01-inputs-and-outputs/first_program/first_program.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main () {
+
+    // std::cout<< "Hello world" <<std::endl;
+    // cout<< "Hello world\n";
+    cout<< "Hello world" <<endl;
+
+    return 0;
+}
