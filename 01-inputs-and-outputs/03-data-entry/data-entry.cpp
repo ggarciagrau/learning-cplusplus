9a6e5e77adf0193f92e8295546c1a781fe612e71
Gerard Garcia Grau <gerardgarciagrau2@gmail.com>
2023-09-14 22:42:24 +0200

    02-03 data entry

---
+++ 01-inputs-and-outputs/03-data-entry/data-entry.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int number;
+
+    cout << "Introduce a number> ";
+    cin >> number;
+
+    cout << "Your number is " << number <<endl;
+
+    return 0;
+}
