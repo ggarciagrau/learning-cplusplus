db7ce64437a9b0d8fa4806661bcb615b4a999ab6
Gerard Garcia Grau <gerardgarciagrau2@gmail.com>
2024-05-18 22:17:19 +0200

    exercise 3

---
+++ 02-expressions/03-exercise/complex-fraction.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int num1, numFrNum, numFrDen, den1, denFrNum, denFrDen, result;
+
+    cout << "Introduce first numerator number> ";
+    cin >> num1;
+
+    cout << "Introduce second numerator numerator> ";
+    cin >> numFrNum;
+    cout << "Introduce second numerator denominator> ";
+    cin >> numFrDen;
+
+    cout << "Introduce first denominator number> ";
+    cin >> den1;
+
+    cout << "Introduce second denominator numerator> ";
+    cin >> denFrNum;
+    cout << "Introduce second denominator denominator> ";
+    cin >> denFrDen;
+
+    result = (num1 + (numFrNum / numFrDen)) / (den1 + (denFrNum / denFrDen));
+
+    cout << "Result: " << result;
+
+    return 0;
+}
