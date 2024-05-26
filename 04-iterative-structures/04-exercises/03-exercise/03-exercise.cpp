313f353b5969e7c50e8319690527349993f0a960
Gerard Garcia Grau <gerardgarciagrau2@gmail.com>
2024-05-26 18:08:21 +0200

    exercise 3

---
+++ 04-iterative-structures/04-exercises/03-exercise/03-exercise.cpp
@@ -0,0 +1,22 @@
+/**
+ * Display in the stdout the sum of the first ten integers greater than 0 
+*/
+
+#include <iostream>
+#include <tgmath.h>
+
+using namespace std;
+
+int main () {
+
+    int result = 0;
+
+    for (int i = i; i <= 10; i++)
+    {
+        result += pow(i, 2);
+    }
+
+    cout << "Result: " << result << endl;
+
+    return 0;
+}
