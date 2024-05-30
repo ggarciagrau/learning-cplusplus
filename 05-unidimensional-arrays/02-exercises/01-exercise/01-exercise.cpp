152339c3b9c664699645f240bce9463d5c822053
Gerard Garcia Grau <gerardgarciagrau2@gmail.com>
2024-05-30 22:45:08 +0200

    exercise 1

---
+++ 05-unidimensional-arrays/02-exercises/01-exercise/01-exercise.cpp
@@ -0,0 +1,16 @@
+/**
+ * Write a program that defines an array of numbers and calculate the sum of it's elements
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    int numbers[] = {4, 5, 6, 9};
+
+    cout << "Result: " << numbers[0] + numbers[1] + numbers[2] + numbers[3] << endl;
+
+    return 0;
+}
